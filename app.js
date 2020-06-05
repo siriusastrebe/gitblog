caf3459579f054390cd689f8de7cfbec666be92c
sirius strebe <siriusastrebe@gmail.com>
2020-06-05 02:36:00 -0700

    Initial commit, markdown->html working, but no scraping yet.

---
+++ app.js
@@ -0,0 +1,14 @@
+const markdown = require('markdown').markdown;
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const html = markdown.toHTML('#Hello\nBeautiful *World*!');
+  res.send("<html><body>" + html + "</body></html>");
+});
+
+app.listen(port, () => console.log(`Gitblog running on http://localhost:${port}`))
+
+
+
